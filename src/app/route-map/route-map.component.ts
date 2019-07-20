df2b42843c252b769bbc7330f1b33922592f420c
Lasimasn <lasisn13@in.ibm.com>
2019-07-20 23:04:46 +0530

    initial commit

---
+++ src/app/route-map/route-map.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { DonationService } from '../donation.service';
+
+@Component({
+  selector: 'app-route-map',
+  templateUrl: './route-map.component.html',
+  styleUrls: ['./route-map.component.css']
+})
+export class RouteMapComponent implements OnInit {
+
+  constructor(
+    private service : DonationService
+  ) { }
+  username : string
+  deliveryRoute : any
+  ngOnInit() {
+    this.username = sessionStorage.getItem('username');
+    console.log(this.username)
+    this.service.fetchDeliveryBoyRoute(this.username).subscribe(data=>{
+      console.log(data)
+      this.deliveryRoute=data;})
+  }
+  onPicked(restaurantUsername: string) {
+    this.service.saveRestaurantLogOnPicked(restaurantUsername).subscribe(data => {
+      console.log(data)
+    })
+  }
+  onDelivered(charityUsername: string) {
+    this.service.saveCharityLogOnDelivered(charityUsername).subscribe(data => {
+      console.log(data)
+    })
+  }
+ }
+ 
