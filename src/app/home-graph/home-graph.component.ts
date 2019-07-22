07f089a8ebc78bf9d22f3af4b941ed5a16d68e3a
Rinciya97 <rinciyadafney@gmail.com>
2019-07-22 14:41:32 +0530

    charity, restaurant responsive

--- src/app/home-graph/home-graph.component.ts
+++ src/app/home-graph/home-graph.component.ts
@@ -9,7 +9,7 @@ export class HomeGraphComponent implements OnInit {
   ngOnInit() {
     var chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
-      theme: "dark2", // "light1", "light2", "dark1", "dark2"
+      theme: "dark", // "light1", "light2", "dark1", "dark2"
       backgroundColor: "rgba(225,150,150,0.5)",
       title:{
         text: "Donation Analysis"
