1aa15d4ffc396f1be6d81931fe31c94627447214
Lasimasn <lasisn13@in.ibm.com>
2019-07-22 20:00:57 +0530

    changed login

--- src/app/home-graph/home-graph.component.ts
+++ src/app/home-graph/home-graph.component.ts
@@ -9,7 +9,8 @@ export class HomeGraphComponent implements OnInit {
   ngOnInit() {
     var chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
-      theme: "dark", // "light1", "light2", "dark1", "dark2"
+      theme: "dark",
+      // "light1", "light2", "dark1", "dark2"
       backgroundColor: "rgba(225,150,150,0.5)",
       title:{
         text: "Donation Analysis"
@@ -23,13 +24,13 @@ export class HomeGraphComponent implements OnInit {
         legendMarkerColor: "grey",
         // legendText: "MMbbl = one million barrels",
         dataPoints: [      
-          { y: 300878, label: "Kerla" },
+          { y: 300878, label: "Kerala" },
           { y: 266455,  label: "Odisha" },
           { y: 169709,  label: "Maharashtra" },
           { y: 158400,  label: "Tamil Nadu" },
           { y: 142503,  label: "Andhra Pradesh" },
           { y: 101500, label: "Karnataka" },
-          { y: 97800,  label: "Gujrat" },
+          { y: 97800,  label: "Gujarat" },
           { y: 80000,  label: "Delhi" }
         ]
       }]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,24 +11,23 @@ import { DataService } from '../data.service';
 })
 export class LoginComponent implements OnInit {
 
-  public user : User;
+  public user: User;
 
-  constructor(private service:LoginService, private route:Router, private data: DataService) { }
+  constructor(private service: LoginService, private route: Router, private data: DataService) { }
 
   ngOnInit() {
-    this.data.changeMessage("login");
   }
-  login(username, password){
-    console.log(username,password);
-    this.user = new User(username,password);
+  login(username, password) {
+    console.log(username, password);
+    this.user = new User(username, password);
     console.log(this.user)
-    this.service.login(this.user).subscribe(data =>{
+    this.service.login(this.user).subscribe(data => {
       console.log(data);
-      sessionStorage.setItem('token',data.token);
-      sessionStorage.setItem('username',data.userDetails.username);
-      sessionStorage.setItem('role',data.userDetails.role);
-      this.data.changeMessage("true");
+      sessionStorage.setItem('token', data.token);
+      sessionStorage.setItem('username', data.userDetails.username);
+      sessionStorage.setItem('role', data.userDetails.role);
+      // this.data.changeMessage(true);
       this.route.navigateByUrl("/DashboardComponent");
-      });
+    });
   }
 }
